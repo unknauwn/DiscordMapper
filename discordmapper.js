3bf5081aecb65e4a507d4ca895f28579a5b12e4d
unknauwn <31632906+unknauwn@users.noreply.github.com>
2020-07-27 20:30:27 +0200

    update url var

--- discordmapper.js
+++ discordmapper.js
@@ -9,7 +9,7 @@ bot.on('ready', function () {
 
 bot.login(process.env.token)
 
-var url = 'https://api.discordmapper.com/api-bot.php';
+var url = process.env.url_api;
 var headers = {
 	'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:24.0) Gecko/20100101 Firefox/24.0',
 	'Content-Type' : 'application/x-www-form-urlencoded'
